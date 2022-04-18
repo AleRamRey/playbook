941b67e6e2136bc91ebc03ee3b4fa662a669b510
AleRamRey <99215646+AleRamRey@users.noreply.github.com>
2022-04-18 14:36:29 -0500

    se agrega linea 6 para comprobar prueba

--- weekly_mission_2/5-pruebas-unitarias/pokemon.test.js
+++ weekly_mission_2/5-pruebas-unitarias/pokemon.test.js
@@ -2,5 +2,6 @@ import Pokemon from './pokemon'
 
 test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+  expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+  //expect(4+2).toBe(6); // argumentos minimos de una prueba
 });
